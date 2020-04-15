bf7b0fc4f2f4c7e6b5516d2c75e462ac66d77db4
Timothy Cao <tcao.pro@gmail.com>
2020-04-14 23:28:40 -0400

    capture photo functionality

--- Components/Camera.js
+++ Components/Camera.js
@@ -1,9 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Text, View, TouchableOpacity } from 'react-native';
 import { Camera } from 'expo-camera';
 
 const CameraComponent = props => {
   const [hasPermission, setHasPermission] = useState(null);
+  const [photo, setPhoto] = useState(undefined);
+
+  const camera = useRef();
 
   useEffect(() => {
     (async () => {
@@ -12,15 +15,26 @@ const CameraComponent = props => {
     })();
   }, []);
 
+  const capturePhoto = async () => {
+    const capturedPhoto = await camera.current.takePictureAsync();
+    setPhoto({ photo: capturedPhoto });
+  };
+
   if (hasPermission === null) {
     return <View />;
   }
   if (hasPermission === false) {
     return <Text>No access to camera</Text>;
   }
+  console.log(photo);
   return (
     <View style={{ flex: 1 }}>
-      <Camera style={{ flex: 1 }} type={Camera.Constants.Type.back}>
+      <Camera
+        style={{ flex: 1 }}
+        type={Camera.Constants.Type.back}
+        ref={ref => {
+          camera.current = ref;
+        }}>
         <View
           style={{
             flex: 1,
@@ -33,7 +47,7 @@ const CameraComponent = props => {
               alignSelf: 'flex-end',
               alignItems: 'center',
             }}
-            onPress={() => {}}>
+            onPress={capturePhoto}>
             <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> Capture </Text>
           </TouchableOpacity>
         </View>
